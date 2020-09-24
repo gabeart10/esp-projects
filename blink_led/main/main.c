1484e94296b13d55a4974dd4def842d65e948475
Gabe Miller <millergabe10@gmail.com>
2020-09-23 23:01:57 -0400

    save

--- blink_led/main/main.c
+++ blink_led/main/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 
+#define BLINK_GPIO 5
 
 void app_main(void)
 {
-    int i = 0;
+    gpio_reset_pin(BLINK_GPIO);
+    
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
     while (1) {
-        printf("[%d] Hello world!\n", i);
-        i++;
-        vTaskDelay(5000 / portTICK_PERIOD_MS);
+      printf("Turn Off LED\n");
+      gpio_set_level(BLINK_GPIO, 0);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      printf("Turn On LED\n");
+      gpio_set_level(BLINK_GPIO, 1);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
